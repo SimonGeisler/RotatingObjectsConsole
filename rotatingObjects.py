f8d19c3f4053010d0f9962801fd82d1f2ebc9034
SimonGeisler <33724461+SimonGeisler@users.noreply.github.com>
2020-10-18 18:59:43 +0200

    Update rotatingObjects.py

--- rotatingObjects.py
+++ rotatingObjects.py
@@ -7,15 +7,16 @@ import numpy
 #We define a cube by three vectors (-> 3x3 matrix) and then use rotation matrices on this matrix.
 #Then we project the  3x3 matrix into 2D and print it
 
-rotationsteps=100000
+rotationsteps=1000
+phiangle=5*(2*3.14)/360
 cubelength=10 #10 characters in the console
-SimpleCubeMatrix = [[1,0,0],[0,1,0],[0,0,1]]
+SimpleCubeMatrix = np.array([[1,0,0],[0,1,0],[0,0,1]])
 rotationangle=math.pi/2 #90 degrees
 rotationaxis= [1,0,0]
 pointchar = 'o'
 
-def rotationmatrix(phi, rax):
-    return \
+def rotationmatrix(phi, rax): #rax: rotationaxis
+    return np.array( \
     [[math.cos(phi) + rax[0]**2 *(1-math.cos(phi))], \
      [rax[0]*rax[1]*(1-math.cos(phi) - rax[2]*math.sin(phi))], \
      [rax[0]*rax[2]*(1-math.cos(phi)+rax[1]*math.sin(phi))]] \
@@ -24,7 +25,7 @@ def rotationmatrix(phi, rax):
       [rax[1]*rax[2]*(1-math.cos(phi))-rax[0]*math.sin(phi)]] \
     ,[[rax[2]*rax[1]*(1-math.cos(phi)) - rax[1]*math.sin(phi)], \
       [rax[2]*rax[1]*(1-math.cos(phi))+rax[0]*math.sin(phi)], \
-      [math.cos(phi)+rax[2]**2 *(1-math.cos(phi))]]
+      [math.cos(phi)+rax[2]**2 *(1-math.cos(phi))]])
 
     
 def getprintstring(dimensionx,dimensiony,vectorlist, pointchar, emptychar=" "):
@@ -37,13 +38,16 @@ def getprintstring(dimensionx,dimensiony,vectorlist, pointchar, emptychar=" "):
     stringbuilder=""
     for(y in range(0,dimensionx)):
         for(x in range(0,dimensiony)):
-            stringbuilder += str(stringmatrix[x,y])
+            stringbuilder += str(stringmatrix[x,y]*100)
         stringbuilder += "\n"
     return stringbuilder
                                  
 clear = lambda: os.system("cls")
 
-#Printing
-for x in range(rotationsteps):
-    print(str(x) + "ms")
+for(x in range(0,rotationsteps):
+    vectorlist = [[]]
+    for(v in range(0,3)):
+          SimpleCubeMatrix[v,:] = np.matmul(rotationmatrix(phiangle,rotationaxis),np.array(SimpleCubeMatrix[v,:]))
+          vectorlist.append(SimpleCubeMatrix[v,:])
+    printf(getprintstring(100,100,vectorlist, "x", " ")
     clear()
